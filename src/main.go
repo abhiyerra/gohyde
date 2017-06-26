4ef74dcf30dd5263fc313639214c088bfc04e936
rjrhaverkamp <rhaverkamp@dcnext.nl>
2017-06-26 13:09:19 +0200

    split file in chuncks of 140 chars

--- src/main.go
+++ src/main.go
@@ -5,17 +5,40 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
-func encode(path *string) (content string, err error) {
+func removeCharacters(input string, characters string) string {
+	filter := func(r rune) rune {
+		if strings.IndexRune(characters, r) < 0 {
+			return r
+		}
+		return -1
+	}
+
+	return strings.Map(filter, input)
+
+}
+
+func encode(path *string) (content []string, err error) {
 	filepath := *path
 	file, err := ioutil.ReadFile(filepath)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	base64File := base64.StdEncoding.EncodeToString(file)
-	return base64File, nil
+	res := ""
+	var fileslice []string
+	for i, r := range base64File {
+		res = res + string(r)
+		if i > 0 && (i+1)%140 == 0 {
+			fileslice = append(fileslice, res)
+			base64File = removeCharacters(base64File, res)
+		}
+	}
+	fileslice = append(fileslice, base64File)
+	return fileslice, nil
 }
 
 func decode(url *string) {
@@ -26,5 +49,7 @@ func main() {
 	flag.Parse()
 
 	file, _ := encode(filename)
-	fmt.Print(file)
+	for _, k := range file {
+		fmt.Print(k)
+	}
 }
